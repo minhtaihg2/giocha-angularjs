888b77cc754dc46c52fb4eb60a0ac9b09b3da244
Harvey Pham <harvey.pham@qsoftvietnam.com>
2016-06-27 10:56:12 +0700

    first commit for yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name giochaClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the giochaClientApp
+ */
+angular.module('giochaClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
