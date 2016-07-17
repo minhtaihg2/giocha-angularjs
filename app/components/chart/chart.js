6a0b4784978a1e091f500d646841b0e435ac02c1
Bui Van Tuyen <tuyenbv@qsoft.com.vn>
2016-07-18 04:40:47 +0700

    chart

--- app/components/chart/chart.js
+++ app/components/chart/chart.js
@@ -9,16 +9,20 @@
  */
 
 gioChaApp.controller('ChartCtrl', ChartCtrl);
-ChartCtrl.$inject = ['$scope', '$http'];
-function ChartCtrl($scope, $http) {
+ChartCtrl.$inject = ['$scope', '$http', 'BaseService', 'settingsUrl'];
+function ChartCtrl($scope, $http, BaseService, settingsUrl) {
     //See: https://github.com/pablojim/highcharts-ng
 //    var myapp = angular.module('myapp', ["highcharts-ng"]);
 //    myapp.controller('myctrl', function ($scope) {
+
+    var _urlOrders = settingsUrl.baseApiUrl + '/orders';
+
     /**
      * 
      * @returns {no}
      * Xoa 1 seri
      */
+
     $scope.removeRandomSeries = function () {
         var seriesArray = $scope.highchartsNG.series
         var rndIdx = Math.floor(Math.random() * seriesArray.length);
@@ -38,89 +42,144 @@ function ChartCtrl($scope, $http) {
         }
     }
 
-    $http({
-        method: 'GET',
-        url: 'data.json'
-    }).then(function successCallback(response) {
-        var data = response.data.data;
-        var arr_data = [];
-       
-        data.forEach(function (val, index, arr) {
-           var date_origin=(val[0]);
-            var res = date_origin.split(",");
-//             console.log(Date.UTC(val[0]));res[0]
-            var order_date=Date.UTC(res[0], res[1], res[2]);
-//            console.log(order_date);
-//            console.log(Date.UTC(val[0]))
-            arr_data.push([order_date, val[1]]);
-        });
-        console.log(arr_data);
-        $scope.highchartsNG = {
-            options: {
-                chart: {
-                    type: 'line'
+
+
+
+
+    $scope.getChart = function () {
+
+
+        var dates = [
+            {"2016-07-13T17:00:01.000Z": 2000},
+            {"2016-07-13T17:00:01.000Z": 1200},
+            {"2016-08-18T17:00:01.000Z": 1300},
+            {"2016-07-14T17:00:01.000Z": 550},
+            {"2016-07-14T17:00:01.000Z": 1000}
+        ];
+
+        function calc(dates) {
+            var response = {};
+            dates.forEach(function (d) {
+                for (var k in d) {
+//                    console.log(k);
+                    var _ = k.split("-");
+                    var month = _[1]
+                    var day = _[2].slice(0, 2);
+
+                    if (!response[month])
+                        response[month] = {}
+                    response[month][day] = response[month][day] ? response[month][day] + d[k] : d[k];
                 }
-            },
-            series: [
-                {
-                    name: 'banh giay',
-                    data: arr_data
-                },
-                {
-                    name: 'banh my cha',
-                    data: [
-                        [Date.UTC(2015, 9, 21), 100.000],
-                        [Date.UTC(2015, 10, 4), 100.000],
-                        [Date.UTC(2015, 10, 9), 700.000],
-                        [Date.UTC(2015, 10, 27), 370.000],
-                        [Date.UTC(2015, 11, 2), 400.000],
-                        [Date.UTC(2015, 11, 26), 100.000],
-                        [Date.UTC(2015, 11, 29), 600.000],
-                        [Date.UTC(2016, 0, 11), 190.000],
-                        [Date.UTC(2016, 0, 26), 700.000],
-                        [Date.UTC(2016, 1, 3), 200.000],
-                        [Date.UTC(2016, 1, 11), 200.000],
-                        [Date.UTC(2016, 1, 25), 100.000],
-                        [Date.UTC(2016, 2, 11), 800.000],
-                        [Date.UTC(2016, 3, 11), 900.000],
-                        [Date.UTC(2016, 4, 1), 500.000],
-                        [Date.UTC(2016, 4, 5), 200.000],
-                        [Date.UTC(2016, 4, 19), 500.000],
-                        [Date.UTC(2016, 5, 3), 180.000]
-                    ],
-                }
-            ],
-            xAxis: {
-                type: 'datetime',
-                tickInterval: 7 * 24 * 3600 * 1000, // one week
-                dateTimeLabelFormats: {// don't display the dummy year
-                    month: '%e. %b',
-                    year: '%b'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Tong tieng(nghin VND)'
-                },
-                min: 0
-            },
-            title: {
-                text: 'Thong ke ban'
-            },
-            loading: false
+            });
+//            console.log(response);
+            return response;
         }
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-    });
-    /**
-     * cac option can thiet
-     */
+
+
+
+        //Call service to get all product
+        BaseService.get(_urlOrders).then(
+                //Success
+                        function (response) {
+                            if (response.status === 'success') {
+                                $scope.orders = response.data;
+                                var arr_data = [];
+
+                                $scope.orders.forEach(function (val, index, arr) {
+                                    if (val.Products.length > 0) {
+                                        var total = 0;
+                                        angular.forEach(val.Products, function (item, key) {
+                                            total += item.price * item.orderProduct.quantity;
+                                        })
+                                        var object = {};
+                                        object[val.orderedAt] = total;
+                                        arr_data.push(object);
+                                    }
+                                });
+                                var arr_data2 = calc(arr_data);
+                                console.log(arr_data2);
+                                var arr_data3 = [];
+                                angular.forEach(arr_data2, function (item, key) {
+//                                     console.log(1111,item);
+                                    angular.forEach(item, function (item2, key2) {
+
+                                        var tempArray = [];
+                                        tempArray.push(Date.UTC(2016, parseInt(key) - 1, parseInt(key2)));
+                                        tempArray.push(item2)
+                                        arr_data3.push(tempArray);
+                                    })
+                                })
+                                console.log(arr_data3);
+                                $scope.highchartsNG = {
+                                    options: {
+                                        chart: {
+                                            type: 'line'
+                                        }
+                                    },
+                                    series: [
+                                        {
+                                            name: 'banh giay',
+                                            data: arr_data3
+                                        },
+                                        {
+                                            name: 'banh my cha',
+                                            data: [
+                                                [Date.UTC(2015, 9, 20), 200.000],
+                                                [Date.UTC(2015, 10, 4), 100.000],
+                                                [Date.UTC(2015, 10, 9), 700.000],
+                                                [Date.UTC(2015, 10, 27), 370.000],
+                                                [Date.UTC(2015, 11, 2), 400.000],
+                                                [Date.UTC(2015, 11, 26), 100.000],
+                                                [Date.UTC(2015, 11, 29), 600.000],
+                                                [Date.UTC(2016, 0, 11), 190.000],
+                                                [Date.UTC(2016, 0, 26), 700.000],
+                                                [Date.UTC(2016, 1, 3), 200.000],
+                                                [Date.UTC(2016, 1, 11), 200.000],
+                                                [Date.UTC(2016, 1, 25), 100.000],
+                                                [Date.UTC(2016, 2, 11), 800.000],
+                                                [Date.UTC(2016, 3, 11), 900.000],
+                                                [Date.UTC(2016, 4, 1), 500.000],
+                                                [Date.UTC(2016, 4, 5), 200.000],
+                                                [Date.UTC(2016, 4, 19), 500.000],
+                                                [Date.UTC(2016, 5, 3), 180.000]
+                                            ],
+                                        }
+                                    ],
+                                    xAxis: {
+                                        type: 'datetime',
+                                        tickInterval: 7 * 24 * 3600 * 1000, // one week
+                                        dateTimeLabelFormats: {// don't display the dummy year
+                                            month: '%e. %b',
+                                            year: '%b'
+                                        },
+                                        title: {
+                                            text: 'Date'
+                                        }
+                                    },
+                                    yAxis: {
+                                        title: {
+                                            text: 'Tong tieng(nghin VND)'
+                                        },
+                                        min: 0
+                                    },
+                                    title: {
+                                        text: 'Thong ke ban'
+                                    },
+                                    loading: false
+                                }
+                            }
+                        },
+                        //Fail
+                                function (error) {
+                                    console.log(error);
+                                });
+                    };
+
+            $scope.getChart();
+
+
 
 
 //
 //    });
-}
+        }
